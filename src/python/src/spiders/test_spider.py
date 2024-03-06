237591d14b496b7c4d0cd3a890843b27db7ee25d
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-03-06 11:34:01 +0200

    fix: change parse id function

---
+++ src/python/src/spiders/test_spider.py
@@ -0,0 +1,15 @@
+import json
+import scrapy
+from utils import ParseWorkHours, ParseCustomerReviews, ParseManagement, ParseSocialMedia, ParseID, ParseAddress
+
+
+
+class TestSpider(scrapy.Spider):
+    name = "test_spider"
+    allowed_domains = ["bbb.org"]
+    start_urls = ["https://www.bbb.org/us/tx/san-antonio/profile/auto-repair/dynamic-car-service-0825-90105933"]
+
+    def parse(self, response):
+        test = ParseID.parse_id(response.url)
+        print(test)
+        pass
